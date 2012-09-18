68a324b01e6102adc15b7d7ab4ea2fd2481b1c4f
Greg Freeman <gregfreeman76@gmail.com>
2012-09-17 23:20:28 -0500

    clean up

--- study/medium_image_analyze.m
+++ study/medium_image_analyze.m
@@ -1,6 +1,5 @@
-%mediume_image_analyze
+%medium_image_analyze
 
-% testRunnerCollectSave('results\medium_image_runner_results_thwompeceutexasedu_20111202T163026')
 load results\medium_image_runner_results_thwompeceutexasedu_20111202T163026\results
 load results\medium_image_runner_results_thwompeceutexasedu_20111202T163026\paramset
 
@@ -10,10 +9,3 @@ set(0,'DefaultLineLineWidth',2);
 [resultsA, paramsetA]=meanExperimentData(results,paramset,'image', {'ms_ssim','ssim','mse','relative_error','runtime'});
 paramsetA=multivariateParameters(paramsetA);
 compareExperiment(resultsA,paramsetA,'delta',{'ssim', 'ms_ssim','mse','relative_error'},[],{'Interpreter','none'})
-subplot(3,1,1)
-xlabel('$\delta$','Interpreter','latex')
-subplot(3,1,2)
-xlabel('$\delta$','Interpreter','latex')
-subplot(3,1,3)
-xlabel('$\delta$','Interpreter','latex')
-% set(gca,'YDir','reverse')
