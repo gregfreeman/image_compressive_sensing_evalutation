731052d97211bce4a986c0a19a01c5311a63374f
Greg Freeman <greg@freemanscience.com>
2013-06-13 10:10:24 -0500

    fix experiment

--- study/small_image_lasso_runner_msrc.m
+++ study/small_image_lasso_runner_msrc.m
@@ -1,4 +1,4 @@
-function medium_image_lasso_runner
+function small_image_lasso_runner
 
 param1.field='delta';
 param1.values=num2cell(0.05:0.05:1);
@@ -12,10 +12,13 @@ param3.values= {'unstructured','coarse_fine'};
 param4.field='image';
 param4.values=num2cell(1:30);
 
-param5.field='image_category';
-param5.values={1};
+param5.field='image_options';
+param5.values={{'cropsize',[128 128]}};
 
-paramset=[param1,param2,param3,param4,param5];
+param6.field='image_category';
+param6.values={1};
+
+paramset=[param1,param2,param3,param4,param5,param6];
 
 events=struct();
 events.runExperiment=@Experiment;
