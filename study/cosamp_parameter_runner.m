c5685c914dd6e184326693bec238eba4ad7b0bdc
Greg Freeman <greg@freemanscience.com>
2013-07-23 22:02:22 -0500

    update cosamp tuning

--- study/cosamp_parameter_runner.m
+++ study/cosamp_parameter_runner.m
@@ -2,7 +2,7 @@ function cosamp_parameter_runner
 
 
 param1.field='delta';
-param1.values=num2cell([0.05,0.1,0.2:0.2:1]);
+param1.values=num2cell(0.05:0.05:0.95);
 
 param2.field='reconstruct';
 param2.values= {'cosamp'};
@@ -14,7 +14,7 @@ param5.field='cosamp_maxiterations';
 param5.values={50,100,150};
 
 param6.field='cosamp_K_factor';
-param6.values={0.1,0.2,0.25,0.3,0.35};
+param6.values={0.1,0.2,0.25,0.3,0.35,0.5};
 
 param7.field='cosamp_tol';
 param7.values={1e-2,1e-3};
